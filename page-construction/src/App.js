1aa91339822a0506f5b3c8aa1b7271acc7811108
giannishorgos <giannishorgos@gmail.com>
2023-01-09 22:08:23 +0200

    basic animation

--- page-construction/src/App.js
+++ page-construction/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import coin from './assets/coin.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='truck-container'>
+            <img src={coin} className='truck' alt='logo' />
+        </div>
+    );
 }
 
 export default App;
