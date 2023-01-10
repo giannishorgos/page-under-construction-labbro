58836c1a113c3121c1d4e3de94317ba162419293
Ioannis Horgos <jhor@acta.com.gr>
2023-01-10 15:14:38 +0200

    under construction page

--- page-construction/src/App.js
+++ page-construction/src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import coin from './assets/coin.png';
+import logo from "./logo.svg";
+import "./App.css";
+import coin from "./assets/coin.png";
+import vroom from "./assets/Vroom Logo.png";
+import labbro from "./assets/LabBro_Logo.png";
+import orana from "./assets/orana.png";
+import shadow from "./assets/shadow.png";
 
 function App() {
     return (
-        <div className='truck-container'>
-            <img src={coin} className='truck' alt='logo' />
+        <div className="container">
+            <div className="logos">
+                <img src={vroom} alt="vroom" />
+                <img src={labbro} alt="labbro" />
+            </div>
+            <div className="coin-container">
+                <h3>Lab Bro under Construction</h3>
+                <img src={coin} className="coin" alt="logo" />
+                <img src={shadow} className="shadow" alt="shadow" />
+            </div>
+            <div className="developer-team">
+                <h3>POWERED BY: </h3>
+                <img src={orana} alt="orana" />
+            </div>
         </div>
     );
 }
